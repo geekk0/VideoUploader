802f2564be37e23db3b06e4b1825fdd06d83e09c
gekk0 <gekk0dw@gmail.com>
2022-08-03 18:48:01 +0300

    codacy fixes v3

--- videofiles/forms.py
+++ videofiles/forms.py
@@ -3,51 +3,6 @@ from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
 
 
-class RegistrationForm(forms.ModelForm):
-
-    phone = forms.CharField(required=False)
-    email = forms.EmailField(required=False)
-    password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
-    confirm_password = forms.CharField(label='Повторите пароль', widget=forms.PasswordInput)
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self.fields['username'].label = 'Логин'
-        self.fields['password'].label = 'Пароль'
-        self.fields['confirm_password'].label = 'Подтвердите пароль'
-        self.fields['phone'].label = 'Номер телефона'
-        self.fields['email'].label = 'Email'
-
-    def clean_email(self):
-        email = self.cleaned_data['email']
-
-        if User.objects.filter(email=email).exists() and self.cleaned_data['email'] != '':
-            raise forms.ValidationError('Данный Email уже зарегистрирован')
-        return email
-
-    def clean_username(self):
-
-        username = self.cleaned_data['username']
-
-        if User.objects.filter(username=username).exists():
-            raise forms.ValidationError('Пользователь с данным именем уже зарегистрирован')
-        return username
-
-    def clean(self):
-
-        password = self.cleaned_data['password']
-        confirm_password = self.cleaned_data['confirm_password']
-
-        if password != confirm_password:
-            raise forms.ValidationError('Пароли не совпадают')
-        return self.cleaned_data
-
-    class Meta:
-        model = User
-        fields = ['username', 'password', 'confirm_password', 'email', 'phone']
-
-
 class LoginForm(forms.ModelForm):
 
     password = forms.CharField(widget=forms.PasswordInput)
